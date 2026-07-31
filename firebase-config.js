04e9266e00df7c5608435d129842f8b302e52d35
Infinitely_Traumatised IT <almaleko2022@gmail.com>
2026-07-31 23:03:36 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Replace every value with the Web app configuration from Firebase Console.
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDx7BmxSRn65tyE0otbEVWKacuILVKXmh0',
+    authDomain: 'maleko-graphic-agency.firebaseapp.com',
+    projectId: 'maleko-graphic-agency',
+    storageBucket: 'maleko-graphic-agency.firebasestorage.app',
+    messagingSenderId: '1039077746041',
+    appId: '1:1039077746041:web:02292ffa84b51daa493ba8'
+};
+
+// This is the UID of the only Firebase Authentication user allowed to manage slides.
+export const adminUid = 'hNwwH6TTtHSlUmanfAQPYSBtMqd2';
